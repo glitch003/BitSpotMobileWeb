3e528c05dd248acefbc9808e726c269f77c67a53
bitspot <bitspot@charitybits.com>
2013-04-23 00:48:05 -0400

    removed dead code

--- includes/scripts.php
+++ includes/scripts.php
@@ -6,14 +6,4 @@
         <script src="js/vendor/bootstrap.min.js"></script>
 
         <script src="js/main.js"></script>
-        
-        <script src="js/jquery.validate.js"></script>
-		
-        <!--
-        <script>
-            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-            s.parentNode.insertBefore(g,s)}(document,'script'));
-        </script>
-        -->
+       
